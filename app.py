ec38628a02b8fb66cfe825bf4335fb291ca72fad
tehreemzirwah-2005 <tehreemzirwah@gmail.com>
2026-09-12 08:38:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ with col2:
                     # Model name exactly updated for Groq
                     llm = ChatGroq(
                         api_key=GROQ_API_KEY, 
-                        model="llama-3.1-8b-instant"
+                        model="openai/gpt-oss-120b"
                     )
                     
                     system_prompt = (
